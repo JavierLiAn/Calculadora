9b3e5d0dc1fcf112edd5e5246f7e5e58599686a2
mgg <margomgar35@alu.edu.gva.es>
2023-11-23 20:34:31 +0100

    main

---
+++ Calculadora.java
@@ -0,0 +1,26 @@
+public class Calculadora {
+    int num1 = 5;
+    int num2 = 2;
+    public static void main(String[] args) {
+        sumar();
+        restar();
+        dividir();
+        multiplicar();
+    }
+
+    static void sumar() {
+
+    }
+
+    static void restar() {
+
+    }
+
+    static void dividir() {
+
+    }
+
+    static void multiplicar() {
+
+    }
+}
