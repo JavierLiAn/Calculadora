c9c959194aac92f0ca2dd3caf3fbbce7dec74b17
JavierLiAn <Javier.Liansio@gmail.com>
2023-11-27 17:11:59 +0100

    (/º-º)/

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,5 @@
+
+
 public class Calculadora {
     static int num1 = 5;
     static int num2 = 2;
@@ -5,7 +7,7 @@ public class Calculadora {
         sumar();
         restar(num1,num2);
         dividir();
-        multiplicar();
+        multiplicar(num1, num2);
     }
 
     static void sumar() {
@@ -22,7 +24,13 @@ public class Calculadora {
         //Mar
     }
 
-    static void multiplicar() {
+    static void multiplicar(int numero, int multiplicador) {
         //Javier
+        int resultado = 0;
+        for (int i = 0; i <= multiplicador-1; i++) {
+            resultado += numero;
+        }
+        System.out.println(resultado);
+
     }
 }
